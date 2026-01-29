d7c695d0d807f44d1f5026ddc352f8e8c382fa77
chunguyeny-stack <chunguyeny@gmail.com>
2026-01-29 22:52:21 +0700

    Create Exfive.py

---
+++ chapter1/Exfive.py
@@ -0,0 +1,12 @@
+name = "Chu Nguyễn Yến Nhi"
+age = 19
+average_score = 9.5
+
+print("tên", name, "kiểu", type(name))
+print("tuổi", age, "kiểu", type(age))
+print("điểm trung bình", average_score, "kiểu", type(average_score))
+age_next_year = age + 1
+doubled_score = average_score * 2
+
+print("tuổi năm sau:", age_next_year, "kiểu", type(age_next_year))
+print("điểm nhân đôi:", doubled_score, "kiểu", type(doubled_score))
