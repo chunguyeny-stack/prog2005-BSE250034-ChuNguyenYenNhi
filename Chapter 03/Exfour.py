fe8cce0cdac5889df0f11f88895996c9a4cbedd8
chunguyeny-stack <chunguyeny@gmail.com>
2026-03-05 21:11:48 +0700

    Create Exfour.py

---
+++ Chapter 03/Exfour.py
@@ -0,0 +1,8 @@
+numbers = [7, 3, 7, 9, 5, 1]
+print("Danh sách ban đầu:", numbers)
+numbers.sort()
+print("Sau khi sắp xếp tăng dần:", numbers)
+numbers.reverse()
+print("Sau khi đảo ngược:", numbers)
+count_7 = numbers.count(7)
+print("Số lần xuất hiện của 7:", count_7)
