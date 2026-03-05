d213633e69c796b19d4be22a10678f8f8cbf6eec
chunguyeny-stack <chunguyeny@gmail.com>
2026-03-05 21:04:03 +0700

    Create Extwo.py

---
+++ Chapter 03/Extwo.py
@@ -0,0 +1,6 @@
+ban_be = ["Linh", "Hoa", "Hùng"]
+print("Danh sách ban đầu:", ban_be)
+ban_be.append("Mai")
+print("Sau khi thêm bạn mới:", ban_be)
+ban_be.pop(1)   
+print("Sau khi xóa bạn thứ hai:", ban_be)
