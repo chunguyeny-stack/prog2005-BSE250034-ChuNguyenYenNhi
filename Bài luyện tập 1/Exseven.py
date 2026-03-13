baa3f022ea711e897891d156cda6323ef5fb44f9
chunguyeny-stack <chunguyeny@gmail.com>
2026-03-13 17:48:23 +0700

    Create Exseven.py

---
+++ Bài luyện tập 1/Exseven.py
@@ -0,0 +1,8 @@
+class Student:
+ def __init__(self,name,score):
+  self.name=name
+  self.score=score
+student1=Student("Nhi",9.8)
+student2=Student("Lan",8.9)
+print("Sinh viên 1:",student1.name,"-",student1.score)
+print("Sinh viên 2:",student2.name,"-",student2.score)
