5e0bb5fd3e96093efe21b4df9a6aee7359591e29
chunguyeny-stack <chunguyeny@gmail.com>
2026-03-13 16:35:35 +0700

    Create Extwo.py

---
+++ Bài luyện tập 1/Extwo.py
@@ -0,0 +1,8 @@
+import math
+a = int(input("Nhập số a: "))
+b = int(input("Nhập số b: "))
+print("lũy thừa a^b: ", a**b)
+print("căn bậc 2 của a: ", math.sqrt(a))
+print("chia lấy phần nguyên: ", a//b)
+print("chia lấy phần dư: ", a%b)
+print("làm tròn số: ", math.ceil(a/b) )
