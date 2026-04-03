e970ecba8cfbbcc8d2fd326917624375bb801000
chunguyeny-stack <chunguyeny@gmail.com>
2026-04-03 18:15:59 +0700

    Refactor book data structure and file writing

--- Luyện tập 14/Exfive.py
+++ Luyện tập 14/Exfive.py
@@ -1,10 +1,23 @@
-book = [
-    "Book 1;30000",
-    "Book 2;50000",
-    "Book 3;100000",
-    "Tong;900000"
-]
+class Book:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+    def get_name(self):
+        return self.name
+    def get_price(self):
+        return self.price
+    def set_price(self, price):
+        self.price = price
+book1 = Book("Book 1", 30000)
+book2 = Book("Book 2", 50000)
+book3 = Book("Book 3", 100000)
+books = [book1, book2, book3]
+tong = sum([b.get_price() for b in books])
 with open("books.txt", "w", encoding="utf-8") as f:
-    for line in book:
-        f.write(line + "\n")
-print("Đã tạo file books.txt thành công!")
+    for b in books:
+        f.write(f"{b.get_name()};{b.get_price()}\n")
+    f.write(f"Tong;{tong}\n")
+print("Đã ghi dữ liệu vào file books.txt thành công!")
+
+
+
